183d4ee49bbf93f9767b28ffcb65c94b690a6725
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-09-25 18:51:35 -0300

    update

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
-const dotenv_1 = require("dotenv");
 const express_1 = __importDefault(require("express"));
 const serverless_http_1 = __importDefault(require("serverless-http"));
 const router_1 = __importDefault(require("./router/router"));
-(0, dotenv_1.config)();
 const app = (0, express_1.default)();
 app.set('view engine', 'ejs');
-app.use(`${process.env.AWS_API_GATEWAY_ENDPOINT}`, router_1.default);
+app.use('*', router_1.default);
 exports.handler = (0, serverless_http_1.default)(app);
 /*
 const port = 1999;
--- router/root/root.js
+++ router/root/root.js
@@ -6,14 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const root = express_1.default.Router();
 root.get('/', (_, res) => {
-    res.status(404).render('root', {
-        data: {
-            page: {
-                status: 404,
-                title: 'Página não Encontrada!',
-                text: 'Você está acessando o site Collge Builder, uma plataforma desenvolvida por alunos para hospedar projetos de alunos da Universidade de Sorocaba. Visite nosso GitHub para ver projetos ou entre em contato para mais informações.',
-            },
-        },
-    });
+    res.status(404).render('root');
 });
 exports.default = root;
